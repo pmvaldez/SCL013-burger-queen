590f00fbd650834e70051056a7d5cc95d0c92f57
pmvaldez <valdezp69@gmail.com>
2020-07-03 14:12:22 -0400

    maquetacion de registro con firebase

--- src/index.js
+++ src/index.js
@@ -2,19 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import firebaseconfig from './firebase/firebase-config';
-import { FirebaseAppProvider } from 'reactfire'
 
 ReactDOM.render((
-  <FirebaseAppProvider firebaseconfig={firebaseconfig}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </FirebaseAppProvider>
   ),document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
