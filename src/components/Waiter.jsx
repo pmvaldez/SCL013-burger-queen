1072259ebb9a44bd3a4fb1bfc81dd8960f216969
pmvaldez <valdezp69@gmail.com>
2020-07-03 18:29:27 -0400

    resolviendo conflictos v2

--- src/components/Waiter.jsx
+++ src/components/Waiter.jsx
@@ -5,11 +5,7 @@ const Waiter = () => {
     return (
         <div>
             <Navbar />
-<<<<<<< HEAD
-            <h1> SOY TERRIBLE waiter</h1>
-=======
             <h1> SOY TERRIBLE waiter 2.0</h1>
->>>>>>> genesis/master
         </div>
     )
 }
