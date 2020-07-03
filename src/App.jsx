9a21d471af42d3a2abf81660160cd6eb29be75fb
GenesisAstete <genesis.astete.a@gmail.com>
2020-07-03 14:33:02 -0400

    login y registro

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Login from './components/Login'
+import Waiter from './components/Waiter'
+import Chef from './components/Chef'
+import Orders from './components/Orders'
+import {auth } from './firebase'
+
+const App = () => {
+  const [firebaseUser, setFirebaseUser] = React.useState(false)
+
+React.useEffect(() => {
+    auth.onAuthStateChanged(user => {
+        console.log(user)
+        if(user){
+            setFirebaseUser(user)
+        }else{
+            setFirebaseUser(null)
+        }
+    })
+}, [])
+
+  return firebaseUser !== false ? (
+        <Router>
+            <div className="container">
+                <Switch>
+                    <Route path="/" exact>
+                      <Login />
+                    </Route>
+                    <Route path="/waiter">
+                        <Waiter />
+                    </Route>
+                    <Route path="/orders">
+                        <Orders />
+                    </Route>
+                    <Route path="/chef">
+                        <Chef />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    ) : (
+      <div>Cargando...</div>
+  )
+}
+
+export default App
---
+++ src/components/Chef.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+
+const Chef = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Chef
