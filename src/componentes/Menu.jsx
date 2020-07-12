e0202a42fa1b4d3b8c96b3fb0e6dbc6ebc05ba30
Nadia080290 <ngallardome@gmail.com>
2020-07-12 00:26:20 -0400

    cambios para generar mas componentes, orden del proyecto

--- src/componentes/Menu.jsx
+++ src/componentes/Menu.jsx
@@ -1,56 +1,12 @@
 import React, {useState} from 'react'
 import '../estilos/menu.css'
 import  info from '../data.json'
-import ResumenPedido from "./ResumenPedido"
-import {db} from '../firebase'
+
 
 const Menu = () => {
     const  data = info.Menu;
     const [type, setType] = useState('breakfast');
-    const [agregar, setAgregar] = React.useState([]);
-    const [sumando, setSumando] = React.useState([]);
-    const [nombre, setNombre] = React.useState('');
-    const [mesa, setMesa] = React.useState('');
   
-    const nombreCliente = (e) => {
-      setNombre(e.target.value);
-    };
-    const numeroMesa = (e) => {
-      setMesa(e.target.value);
-    };
-
-    const pedido= (e) => {
-        const valor = e.target.value;
-        const precioPedido = parseInt(valor);
-        const nombrePedido = e.target.name;
-        
-    //acumulacion de pedido
-    agregar.push(`${nombrePedido} $${precioPedido}`);
-    setAgregar([...agregar]);
-    console.log(agregar)
-   
-
-
-    sumando.push(precioPedido)
-    setSumando([...sumando])
-    } ;
-
-   const suma = sumando.reduce((a, b) => a + b, 0);
-
-   const agregarPedido= async (e) => {
-    e.preventDefault()
-    try {
-        const nuevoPedido = {
-           pedido: agregar
-
-        }
-        const data = await db.collection("pedidos").add(nuevoPedido);
-    } catch (error) {
-        console.log(error)
-    }
-    
-}
-
     return (
         <div className="container">
             <div className="row">  
@@ -74,36 +30,13 @@ const Menu = () => {
           
             <div className="btn-group-vertical">
                 {data.filter(elemen => elemen.type === type ).map((filteredelemen, i) => (
-                    <button onClick={pedido} value={filteredelemen.price} name={filteredelemen.name}className="btn btn-color mt-2" key={i}>{filteredelemen.name} ${filteredelemen.price}</button>
+                    <button /* onClick={} value={filteredelemen.price} name={filteredelemen.name} */className="btn btn-color mt-2" key={i}>{filteredelemen.name} ${filteredelemen.price}</button>
  /*                    <li key={i}>
                         {filteredelemen.name}
                     </li> */
                 ))}
                 </div>
                 </div>
-         <div className="col-auto ctnproductos">      
-        <section className="listaprecios">
-            <aside>
-                    <label> Nombre <input type="text" onChange={nombreCliente} placeholder='Ignacio' value={nombre} /> </label>
-                    <label> N° de Mesa <input className="inputMesa" type="text" onChange={numeroMesa} placeholder='1' value={mesa} /> </label>
-            <div className= "container">
-            <h1 className="text-center ">Pedido</h1>
-            {agregar.map((filteredelemen, i) => {
-                return (
-                  <ul key={i} >
-                    <ResumenPedido nombre={filteredelemen} />
-                  </ul>
-                )
-              })
-            }
-            <h3>Total= ${suma}</h3>
-            <button className="btn btn-dark" type="submit" onClick={agregarPedido}>
-              Enviar
-            </button>
-          </div>
-            </aside>
-      </section>
-        </div>
         </div> 
         </div>  
     )
