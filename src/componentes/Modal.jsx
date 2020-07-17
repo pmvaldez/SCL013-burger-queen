f414a48c1ced06ad4eca4365208a132068210eb3
Nadia080290 <ngallardome@gmail.com>
2020-07-17 03:40:57 -0400

    agregar nav y css

---
+++ src/componentes/Modal.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Modal = () => {
+
+
+
+    return (
+        <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p>¿Estas seguro que quieres cancelar este pedido?.</p>
+                        </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-primary">Aceptar</button>
+                        </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Modal
+
---
+++ src/componentes/NavbarChef.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {NavLink, withRouter} from 'react-router-dom'
+import {auth} from '../firebase'
+import Login from './Login'
+import logo from '../imagen/logo.jpg'
+import outline_power from '../imagen/outline_power.png'
+import '../estilos/navbar.css'
+
+const Navbar = (props) => {
+
+    const cerrarSesion = () => {
+        auth.signOut()
+            .then(() => {
+                props.history.push('/')
+            })
+    }
+
+    return (
+        <div className="navbar navbar-dark bg-dark">
+
+            <h2 className="mr-auto textNav">Cocinero</h2> 
+            <img className="navLogo" alt="logo" src={logo}/>
+            <div>
+                <div className="d-flex ml-auto">
+                    {
+                        props.firebaseUser !== null ? (
+                        <img onClick={() => cerrarSesion()} className="btn btnclose" src={outline_power} alt="" />
+                        ): (
+                        <NavLink 
+                            className="btn btn-dark" 
+                            to="/"
+                        >
+                            <Login />
+                        </NavLink>
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default withRouter(Navbar)
